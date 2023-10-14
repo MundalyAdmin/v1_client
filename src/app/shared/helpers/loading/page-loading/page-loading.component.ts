5e3f00e4579ab0197fa43881964973a778c3edb3
Babacar Kane <babacar@mundaly.com>
2023-10-14 09:20:33 +0000

    Loading done

--- src/app/shared/helpers/loading/page-loading/page-loading.component.ts
+++ src/app/shared/helpers/loading/page-loading/page-loading.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-loading',
   templateUrl: './page-loading.component.html',
-  styleUrls: ['./page-loading.component.scss']
+  styleUrls: ['./page-loading.component.scss'],
 })
 export class PageLoadingComponent implements OnInit {
+  @Input() height = 200;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
