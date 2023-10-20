b583eebe089fb0f10c535794af72e2048b01fb50
Babacar Kane <babacar@mundaly.com>
2023-10-20 13:45:23 +0000

    Loading component updated

--- src/app/shared/helpers/loading/loading/loading.component.ts
+++ src/app/shared/helpers/loading/loading/loading.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loading',
   templateUrl: './loading.component.html',
-  styleUrls: ['./loading.component.scss']
+  styleUrls: ['./loading.component.scss'],
 })
 export class LoadingComponent implements OnInit {
+  @Input() size: number = 16;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
