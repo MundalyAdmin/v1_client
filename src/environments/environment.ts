f9c0830aa4ab889f03d6c1d3c298fe389e62285c
Babacar Kane <babacar@mundaly.com>
2023-10-30 21:46:58 +0000

    deploy correction

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://dolphin-app-huaya.ondigitalocean.app/api/v1',
+  apiUrl: 'https://dolphin-app-huaya.ondigitalocean.app/api/v1',
   // urlEncryptionKey: '@kdiWolsa0ksdfm',
   // moneyToPointRation: 0.9,
 };
