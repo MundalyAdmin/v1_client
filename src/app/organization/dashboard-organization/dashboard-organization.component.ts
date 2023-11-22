d6b0e76e0ddfe6aea4133c8338aac1d470c778c2
Babacar Kane <babacar@mundaly.com>
2023-11-22 21:12:08 +0000

    user profile displayed

--- src/app/organization/dashboard-organization/dashboard-organization.component.ts
+++ src/app/organization/dashboard-organization/dashboard-organization.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-dashboard-organization',
   templateUrl: './dashboard-organization.component.html',
-  styleUrls: ['./dashboard-organization.component.scss']
+  styleUrls: ['./dashboard-organization.component.scss'],
 })
 export class DashboardOrganizationComponent {
-
+  constructor(public authService: AuthService) {}
 }
