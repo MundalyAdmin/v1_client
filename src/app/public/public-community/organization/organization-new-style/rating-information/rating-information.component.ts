aaabb75629e6c89b025ce7b15e46ba001651ae6a
Babacar Kane <babacar@mundaly.com>
2023-12-07 00:08:18 +0000

    First page restructuring done

---
+++ src/app/public/public-community/organization/organization-new-style/rating-information/rating-information.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rating-information',
+  templateUrl: './rating-information.component.html',
+  styleUrls: ['./rating-information.component.scss']
+})
+export class RatingInformationComponent {
+
+}
