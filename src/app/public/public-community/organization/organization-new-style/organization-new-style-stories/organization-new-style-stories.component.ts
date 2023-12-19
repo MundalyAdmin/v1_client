a27f4bba2053025d346b3483508c477055a73b41
Babacar Kane <babacar@mundaly.com>
2023-12-19 17:22:00 +0000

    Cleanup for deployment

--- src/app/public/public-community/organization/organization-new-style/organization-new-style-stories/organization-new-style-stories.component.ts
+++ src/app/public/public-community/organization/organization-new-style/organization-new-style-stories/organization-new-style-stories.component.ts
@@ -1,12 +1,14 @@
 import { Component, AfterViewInit } from '@angular/core';
 import { ViewportScroller } from '@angular/common';
 import { Storage } from '../../../../../shared/helpers/storage/storage';
+import { Flowbite } from '../../../../../shared/decorators/flowbite.decorator';
 
 @Component({
   selector: 'app-organization-new-style-stories',
   templateUrl: './organization-new-style-stories.component.html',
   styleUrls: ['./organization-new-style-stories.component.scss'],
 })
+@Flowbite()
 export class OrganizationNewStyleStoriesComponent implements AfterViewInit {
   constructor(public scroller: ViewportScroller, public storage: Storage) {}
 
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'https://seahorse-app-jjhoh.ondigitalocean.app/',
+  apiUrl: 'https://seahorse-app-jjhoh.ondigitalocean.app/api/v1',
   production: false,
 };
