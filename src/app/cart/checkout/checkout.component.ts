a0a11e6913a3411b67cc2bfbf21de6d981b18511
Babacar Kane <babacar@mundaly.com>
2024-02-02 21:07:20 +0000

    Update 1

--- src/app/cart/checkout/checkout.component.ts
+++ src/app/cart/checkout/checkout.component.ts
@@ -15,7 +15,7 @@ export class CheckoutComponent implements OnInit {
     this.cartService.data = [];
 
     window.open(
-      'https://www.figma.com/proto/6oIZcKuGxg26kcRzq8XJ2U/Mundaly?page-id=0%3A1&type=design&node-id=79-9187&viewport=-381%2C5%2C0.11&t=FQrnUfWTmX7TIHGz-1&scaling=scale-down&starting-point-node-id=79%3A9187&show-proto-sidebar=1&mode=design',
+      'https://www.figma.com/proto/6oIZcKuGxg26kcRzq8XJ2U/Mundaly?page-id=0%3A1&type=design&node-id=79-9187&viewport=-381%2C5%2C0.11&t=FQrnUfWTmX7TIHGz-8&scaling=scale-down&starting-point-node-id=79%3A9187&show-proto-sidebar=1&hide-ui=1',
       '_self'
     );
   }
