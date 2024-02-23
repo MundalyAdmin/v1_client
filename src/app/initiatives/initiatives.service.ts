9f9753735562bb4c63e32851fc6dee9781033604
Babacar Kane <babacar@mundaly.com>
2024-02-23 17:40:47 +0000

    Initiatives updated

--- src/app/initiatives/initiatives.service.ts
+++ src/app/initiatives/initiatives.service.ts
@@ -11,84 +11,84 @@ export class InitiativesService extends BaseMockService<InitiativeImpact> {
   constructor() {
     super([
       {
-        name: 'Tarkwa Breman Community Farm',
+        name: 'Establishing the new Communities and Social Performance mode',
         startDate: new Date(),
         goals: [
           {
-            name: 'Farm Expension',
+            name: 'Onboard Communities and Social Performance professionals',
             metricType: 'Total',
-            metric: 'Acres',
-            startValue: 10,
-            target: 17,
+            metric: 'csps ',
+            startValue: 250,
+            target: 500,
             data: [
               {
-                date: new Date(),
-                data: 1.5,
+                date: new Date('2023-01-01'),
+                data: 88,
                 description: '',
               },
               {
-                date: new Date(),
-                data: 1.7,
+                date: new Date('2023-02-01'),
+                data: 64,
                 description: '',
               },
               {
-                date: new Date(),
-                data: 0.2,
+                date: new Date('2023-03-01'),
+                data: 122,
                 description: '',
               },
               {
-                date: new Date(),
-                data: 5,
+                date: new Date('2023-04-01'),
+                data: 88,
                 description: '',
               },
               {
-                date: new Date(),
-                data: 7,
+                date: new Date('2023-05-01'),
+                data: 128,
                 description: '',
               },
             ],
           },
-          {
-            name: 'Secure Fertilizer from COCOBOD',
-            metricType: 'Total',
-            metric: 'Bags',
-            startValue: 10,
-            target: 57,
-            data: [
-              {
-                date: new Date(),
-                data: 12,
-                description: '',
-              },
-              {
-                date: new Date(),
-                data: 13,
-                description: '',
-              },
-              {
-                date: new Date(),
-                data: 23,
-                description: '',
-              },
-              {
-                date: new Date(),
-                data: 18,
-                description: '',
-              },
-            ],
-          },
+          // {
+          //   name: 'Secure Fertilizer from COCOBOD',
+          //   metricType: 'Total',
+          //   metric: 'Bags',
+          //   startValue: 10,
+          //   target: 57,
+          //   data: [
+          //     {
+          //       date: new Date(),
+          //       data: 12,
+          //       description: '',
+          //     },
+          //     {
+          //       date: new Date(),
+          //       data: 13,
+          //       description: '',
+          //     },
+          //     {
+          //       date: new Date(),
+          //       data: 23,
+          //       description: '',
+          //     },
+          //     {
+          //       date: new Date(),
+          //       data: 18,
+          //       description: '',
+          //     },
+          //   ],
+          // },
         ],
       },
       {
-        name: "Tarkwa Breman Girl's School",
+        name: ' Reducing barriers and increasing Indigenous employment',
         startDate: new Date(),
         goals: [
           {
-            name: 'Free home tuition',
+            name: 'Indigenous business development Investments',
             metricType: 'Total',
-            metric: 'Tuition Fees',
-            startValue: 10,
-            target: 85,
+            metric: 'millions USD',
+            startValue: 0,
+            target: 50,
             data: [
               {
                 date: new Date('2023-03-01'),
@@ -97,56 +97,22 @@ export class InitiativesService extends BaseMockService<InitiativeImpact> {
               },
               {
                 date: new Date('2023-04-01'),
+                data: 9,
+                description: '',
+              },
+              {
+                date: new Date('2023-05-01'),
+                data: 9,
+                description: '',
+              },
+              {
+                date: new Date('2023-06-01'),
+                data: 7,
+                description: '',
+              },
+              {
+                date: new Date('2023-07-01'),
                 data: 15,
-                description: '',
-              },
-              {
-                date: new Date('2023-05-01'),
-                data: 18,
-                description: '',
-              },
-              {
-                date: new Date('2023-06-01'),
-                data: 22,
-                description: '',
-              },
-              {
-                date: new Date('2023-07-01'),
-                data: 16,
-                description: '',
-              },
-            ],
-          },
-          {
-            name: 'Enable access to PPEs to students',
-            metricType: 'Total',
-            metric: 'Students',
-            startValue: 10,
-            target: 340,
-            data: [
-              {
-                date: new Date('2023-03-01'),
-                data: 40,
-                description: '',
-              },
-              {
-                date: new Date('2023-04-01'),
-                data: 40,
-                description: '',
-              },
-              {
-                date: new Date('2023-05-01'),
-                data: 60,
-                description: '',
-              },
-              {
-                date: new Date('2023-06-01'),
-                data: 40,
-                description: '',
-              },
-              {
-                date: new Date('2023-07-01'),
-                data: 70,
                 description: '',
               },
             ],
