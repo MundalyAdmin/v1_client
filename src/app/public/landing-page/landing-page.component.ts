30fbad7eb36f85b45fe5645a740ad0067c5f6071
Babacar Kane <babacar@mundaly.com>
2024-02-28 13:25:39 +0000

    Landing page layout done

--- src/app/public/landing-page/landing-page.component.ts
+++ src/app/public/landing-page/landing-page.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+  styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent {
+  activeTabs: { [key: string]: boolean } = {
+    communityMember: false,
+    organization: true,
+  };
 
+  constructor() {}
+
+  toggleTab(tab: 'communityMember' | 'organization') {
+    Object.keys(this.activeTabs).forEach((key: string) => {
+      this.activeTabs[key] = key === tab ? true : false;
+    });
+  }
 }
