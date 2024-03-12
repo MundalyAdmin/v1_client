a211ba1e6b4601e0f1417ac3a19963e18d08b64b
Babacar Kane <babacar@mundaly.com>
2024-03-12 00:20:07 +0000

    Error fixed

--- src/app/organization/dashboard-organization/dashboard-organization-community/dashboard-organization-community.component.ts
+++ src/app/organization/dashboard-organization/dashboard-organization-community/dashboard-organization-community.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { Flowbite } from '../../../shared/decorators/flowbite.decorator';
 
 @Component({
   selector: 'app-dashboard-organization-community',
   templateUrl: './dashboard-organization-community.component.html',
-  styleUrls: ['./dashboard-organization-community.component.scss']
+  styleUrls: ['./dashboard-organization-community.component.scss'],
 })
-export class DashboardOrganizationCommunityComponent {
-
-}
+@Flowbite()
+export class DashboardOrganizationCommunityComponent {}
--- src/app/organization/dashboard-organization/dashboard-organization-impact/dashboard-organization-impact.component.ts
+++ src/app/organization/dashboard-organization/dashboard-organization-impact/dashboard-organization-impact.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { Flowbite } from '../../../shared/decorators/flowbite.decorator';
 
 @Component({
   selector: 'app-dashboard-organization-impact',
   templateUrl: './dashboard-organization-impact.component.html',
-  styleUrls: ['./dashboard-organization-impact.component.scss']
+  styleUrls: ['./dashboard-organization-impact.component.scss'],
 })
-export class DashboardOrganizationImpactComponent {
-
-}
+@Flowbite()
+export class DashboardOrganizationImpactComponent {}
