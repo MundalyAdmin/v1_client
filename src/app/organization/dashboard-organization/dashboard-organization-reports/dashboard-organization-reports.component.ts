418154e813d6a5b55f6bf75f1db3716a34f62aa8
Babacar Kane <babacar@mundaly.com>
2024-06-06 17:44:51 +0000

    Revert "Super admin and dashboard report done"
    This reverts commit 91021b838ba137c5c5c30a037375e5531cba1284.

--- src/app/organization/dashboard-organization/dashboard-organization-reports/dashboard-organization-reports.component.ts
+++ src/app/organization/dashboard-organization/dashboard-organization-reports/dashboard-organization-reports.component.ts
@@ -1,55 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { BaseComponent } from '../../../shared/base-component';
-import { OrganizationReport } from '../organization-report/organization-report.model';
-import { OrganizationReportService } from '../organization-report/organization-report.service';
-import { OrganizationService } from '../../organization.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-organization-reports',
   templateUrl: './dashboard-organization-reports.component.html',
-  styleUrls: ['./dashboard-organization-reports.component.scss'],
+  styleUrls: ['./dashboard-organization-reports.component.scss']
 })
-export class DashboardOrganizationReportsComponent
-  extends BaseComponent<OrganizationReport>
-  implements OnInit
-{
-  showReportUploadModal = false;
-  constructor(
-    public organizationReportService: OrganizationReportService,
-    public organizationService: OrganizationService
-  ) {
-    super();
-  }
+export class DashboardOrganizationReportsComponent {
 
-  override ngOnInit(): void {
-    this.subscriptions['organization'] =
-      this.organizationService.singleData$.subscribe((organization) => {
-        if (organization) this.getByOrganizationId(organization.id!);
-      });
-  }
-
-  getByOrganizationId(organizationId: number) {
-    this.loading = true;
-    this.organizationReportService
-      .getByOrganizationId(organizationId)
-      .subscribe({
-        next: () => (this.loading = false),
-        error: () => (this.loading = false),
-      });
-  }
-
-  getMinifiedTypeLabel(type: string) {
-    if (type.toLowerCase() === 'custom report') return type;
-    return type
-      .split(' ')
-      .map((word) => {
-        return word.includes('-')
-          ? word
-              .split('-')
-              .map((w) => w[0].toUpperCase())
-              .join('')
-          : word[0].toUpperCase();
-      })
-      .join('');
-  }
 }
