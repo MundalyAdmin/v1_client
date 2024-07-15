7e9dfca2478958242121ba3b0227291356813da3
Unegbu Kingsley <kingsobino@gmail.com>
2024-07-15 12:56:49 +0100

    add text based question

--- src/app/setup/survey/survey.component.ts
+++ src/app/setup/survey/survey.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
-import { tick } from '@angular/core/testing';
 
-interface Question {
+
+type BaseQuestion = {
   title: string
+  type: "optionBased" | "textBased"
+}
+
+type OptionBasedQuestion =  BaseQuestion & {
   chooseMany: boolean
   manyOptionLimited: boolean
   optionLimitCount: number
@@ -17,6 +21,13 @@ interface Question {
   }
 }
 
+type TextBasedQuestion = BaseQuestion & {
+  
+}
+
+type Question = OptionBasedQuestion | TextBasedQuestion
+
+
 @Component({
   selector: 'app-survey',
   templateUrl: './survey.component.html',
@@ -25,6 +36,7 @@ interface Question {
 export class SurveyComponent {
 
   questions: Question[] = [{
+    type: "optionBased",
     title: "",
     chooseMany: false,
     manyOptionLimited: false,
@@ -80,23 +92,37 @@ export class SurveyComponent {
     ]
   }
 
-  newQuestion() {
-    this.questions.push({
-      title: "",
-      chooseMany: false,
-      manyOptionLimited: false,
-      optionLimitCount: 0,
-      randomizeOptions: false,
-      options: [""],
-      other: false,
-      notSure: false,
-      preferNotSay: false,
-      nonOfAbove: false,
-      preview: {
-        otherSelected: false
+  newQuestion(type: "optionBased" | "textBased") : Question{
+    if (type === "optionBased") return (
+      {
+        type: "optionBased",
+        title: "",
+        chooseMany: false,
+        manyOptionLimited: false,
+        optionLimitCount: 0,
+        randomizeOptions: false,
+        options: [""],
+        other: false,
+        notSure: false,
+        preferNotSay: false,
+        nonOfAbove: false,
+        preview: {
+          otherSelected: false
+        }
       }
+    )
+    return ({
+      type: "textBased",
+      title: ""
     })
   }
+
+  addNewQuestion() {
+    this.questions.push(
+      this.newQuestion('optionBased')
+    )
+  }
+
 
   changeQuestion(event: any, index: number) {
     this.questions[index].title = event.target.value
@@ -107,7 +133,8 @@ export class SurveyComponent {
   }
 
   duplicateQuestion(index: number) {
-    this.questions = [...this.questions.slice(0, index + 1), { ...this.questions[index], options: [...this.questions[index].options.map(x => x)]}, ...this.questions.slice(index + 1)];
+    const clonedObject = JSON.parse(JSON.stringify(this.questions[index]))
+    this.questions = [...this.questions.slice(0, index + 1), clonedObject, ...this.questions.slice(index + 1)];
   }
 
   moveUpward(index: number) {
@@ -120,49 +147,90 @@ export class SurveyComponent {
     [this.questions[index], this.questions[index + 1]] = [this.questions[index + 1], this.questions[index]];
   }
 
+  chooseQuestionType(event: any, index: number) {
+    const value = event.target.value;
+    if (this.questions[index].type === value) return;
+
+    this.questions[index] = this.newQuestion(value);
+  }
+
+
+  // activeQuestion() {
+  //   const question = this.questions[this.activeQuestionIndex];
+  //   return this.convertQuestionToType(question)
+  // }
+
+
+  // convertQuestionToType(question: Question){
+  //   switch(question.type) {
+  //     case 'optionBased':
+  //       return (question as OptionBasedQuestion);
+  //     case 'textBased':
+  //       return (question as TextBasedQuestion)
+  //   }
+  // }
+
+
+  /**
+   * 
+   *  Option Based Question Helpers
+   * 
+   */
+
+  
+  asOptionBasedQuestion(question: Question): OptionBasedQuestion{
+    return (question as OptionBasedQuestion)
+  }
+
   selectOptionSelection(index: number, val: boolean) {
-    this.questions[index].chooseMany = val;
+    (this.questions[index] as OptionBasedQuestion).chooseMany = val;
   }
 
   selectOptionManyLimit(event: any, index: number) {
-    this.questions[index].manyOptionLimited = (event.target.value === "true");
+    const question =  (this.questions[index] as OptionBasedQuestion)
+    question.manyOptionLimited = (event.target.value === "true");
   }
 
   updateOptionCount(index: number, count: number) {
-    if ((this.questions[index].optionLimitCount + count) < 0 || (this.questions[index].optionLimitCount + count) > 20) return;
+    const question =  (this.questions[index] as OptionBasedQuestion)
+    if ((question.optionLimitCount + count) < 0 || (question.optionLimitCount + count) > 20) return;
 
-    this.questions[index].optionLimitCount += count;
+    question.optionLimitCount += count;
   }
 
   switchRandomizeOption(index: number) {
-    this.questions[index].randomizeOptions = !this.questions[index].randomizeOptions;
+    const question =  (this.questions[index] as OptionBasedQuestion)
+    question.randomizeOptions = !question.randomizeOptions;
   }
 
   changeOptionText(event: any, index: number, optionIndex: number) {
     const value = event.target.value;
+    const question =  (this.questions[index] as OptionBasedQuestion)
 
-    this.questions[index].options[optionIndex] = value;
+    question.options[optionIndex] = value;
 
-    if (!value && optionIndex < this.questions[index].options.length - 1) {
-      this.questions[index].options = [...this.questions[index].options.slice(0, optionIndex), ...this.questions[index].options.slice(optionIndex + 1)]
+    if (!value && optionIndex < question.options.length - 1) {
+      question.options = [...question.options.slice(0, optionIndex), ...question.options.slice(optionIndex + 1)]
       return;
     }
 
-    if (optionIndex === this.questions[index].options.length - 1 && value) {
-      this.questions[index].options = [...this.questions[index].options, ""];
+    if (optionIndex === question.options.length - 1 && value) {
+      question.options = [...question.options, ""];
     }
 
   }
 
   selectNormalOptions(nOpt: "other" | "notSure" | "preferNotSay" | "nonOfAbove", index: number) {
-    this.questions[index][nOpt] = !this.questions[index][nOpt];
-    if (nOpt === "other" && !this.questions[index][nOpt]){
-      this.questions[index].preview.otherSelected = false
+    const question =  (this.questions[index] as OptionBasedQuestion)
+    question[nOpt] = !question[nOpt];
+    if (nOpt === "other" && !question[nOpt]){
+      question.preview.otherSelected = false
     }
   }
 
   selectPreviewOther(index: number) {
-    this.questions[index].preview.otherSelected = !this.questions[index].preview.otherSelected
+    const question =  (this.questions[index] as OptionBasedQuestion)
+    question.preview.otherSelected = !question.preview.otherSelected
   }
 
   trackByFn(index: number, item: any) {
