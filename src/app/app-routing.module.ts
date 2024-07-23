a9c849b8ca3429fb90bb21aa9fc2e86a7f24e724
Babacar Kane <babacar@mundaly.com>
2024-07-23 07:49:24 +0000

    fix: Redirection to authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,6 +16,7 @@ const routes: Routes = [
       {
         path: '',
         redirectTo: 'auth',
+        pathMatch: 'full',
       },
       {
         path: 'public',
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,4 +1,5 @@
 export const environment = {
   apiUrl: 'https://seahorse-app-jjhoh.ondigitalocean.app/api/v1',
   production: false,
+  googlePlaceAPI: 'AIzaSyAUybOXb6_DDX08hdB-uVit5JlTUgdQwPY',
 };
