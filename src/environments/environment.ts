72438c43800c10ea0a91af90753c7dd3301c9af0
Babacar Kane <babacar@mundaly.com>
2024-09-05 22:56:33 +0000

    fixed backend url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // apiUrl: 'https://seahorse-app-jjhoh.ondigitalocean.app/api/v1',
-  apiUrl: 'https://seahorse-app-jjhoh.ondigitalocean.app/api/v1',
+  apiUrl: 'https://dolphin-app-huaya.ondigitalocean.app/',
   googlePlaceAPI: 'AIzaSyAUybOXb6_DDX08hdB-uVit5JlTUgdQwPY',
   // urlEncryptionKey: '@kdiWolsa0ksdfm',
   // moneyToPointRation: 0.9,
