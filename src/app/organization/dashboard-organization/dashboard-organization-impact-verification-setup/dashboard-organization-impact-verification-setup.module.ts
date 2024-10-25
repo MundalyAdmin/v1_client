3446e54184d32915252620cfd4a33cc2f4f21436
Babacar Kane <babacar@mundaly.com>
2024-10-25 15:50:36 +0000

    update

--- src/app/organization/dashboard-organization/dashboard-organization-impact-verification-setup/dashboard-organization-impact-verification-setup.module.ts
+++ src/app/organization/dashboard-organization/dashboard-organization-impact-verification-setup/dashboard-organization-impact-verification-setup.module.ts
@@ -1,19 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SharedModule } from '../../../shared/shared.module';
 import { NgxGpAutocompleteModule } from '@angular-magic/ngx-gp-autocomplete';
-import { Loader } from '@googlemaps/js-api-loader';
-import { environment } from '../../../../environments/environment';
-import { DashboardOrganizationImpactVerificationSetupComponent } from './dashboard-organization-impact-verification-setup.component';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { DashboardOrganizationImpactVerificationSetupCommunitiesComponent } from '../dashboard-organization-impact-verification-setup-communities/dashboard-organization-impact-verification-setup-communities.component';
-import { DashboardOrganizationImpactVerificationSetupParticipantComponent } from '../dashboard-organization-impact-verification-setup-participant/dashboard-organization-impact-verification-setup-participant.component';
-import { SliderModule } from 'primeng/slider';
-import { DashboardOrganizationImpactVerificationSetupLaunchComponent } from '../dashboard-organization-impact-verification-setup-launch/dashboard-organization-impact-verification-setup-launch.component';
-import { DashboardOrganizationImpactVerificationSetupBaseComponent } from '../dashboard-organization-impact-verification-setup-base/dashboard-organization-impact-verification-setup-base.component';
-import { InputSwitchModule } from 'primeng/inputswitch';
-import { DashboardOrganizationImpactVerificationSetupSurveyComponent } from '../dashboard-organization-impact-verification-setup-survey/dashboard-organization-impact-verification-setup-survey.component';
 import { AutoCompleteModule } from 'primeng/autocomplete';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { SliderModule } from 'primeng/slider';
+import { SharedModule } from '../../../shared/shared.module';
+import { DashboardOrganizationImpactVerificationSetupBaseComponent } from '../dashboard-organization-impact-verification-setup-base/dashboard-organization-impact-verification-setup-base.component';
+import { DashboardOrganizationImpactVerificationSetupCommunitiesComponent } from '../dashboard-organization-impact-verification-setup-communities/dashboard-organization-impact-verification-setup-communities.component';
+import { DashboardOrganizationImpactVerificationSetupLaunchComponent } from '../dashboard-organization-impact-verification-setup-launch/dashboard-organization-impact-verification-setup-launch.component';
+import { DashboardOrganizationImpactVerificationSetupParticipantComponent } from '../dashboard-organization-impact-verification-setup-participant/dashboard-organization-impact-verification-setup-participant.component';
+import { DashboardOrganizationImpactVerificationSetupSurveyComponent } from '../dashboard-organization-impact-verification-setup-survey/dashboard-organization-impact-verification-setup-survey.component';
+import { DashboardOrganizationImpactVerificationSetupComponent } from './dashboard-organization-impact-verification-setup.component';
 
 const routes = [
   {
